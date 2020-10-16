32cfb6bf44f62fe6ecaf181fded3efe4ddb5b6dc
Gvstavo <gustavopaivads@gmail.com>
2020-10-15 23:17:45 -0300

    > I

---
+++ assets/js/atendimento.js
@@ -0,0 +1,253 @@
+import "../css/app.scss"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import DatePicker from "react-datepicker";
+import regeneratorRuntime from "regenerator-runtime";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Dialogo from './dialog.js';
+
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+
+var api = axios.create({
+    headers: { 'X-NZ-Token': window['API_TOKEN'] }
+});
+
+
+class Atendimento extends React.Component {
+
+
+
+	constructor(props){
+	
+		super(props);
+		this.state = {
+			cliente:  '' ,
+			contato: " " ,
+			entrada: new Date(),
+			pago: false,
+			saida: new Date(),
+			servico: " ",
+			valor: 0.0,
+      obs: " ",
+      arr: [],
+		};
+
+		this.handleChangeClient = this.handleChangeClient.bind(this);
+		this.handleChangeContato = this.handleChangeContato.bind(this);
+		this.handleChangeEntrada = this.handleChangeEntrada.bind(this);
+		this.handleChangePago = this.handleChangePago.bind(this);
+		this.handleChangeSaida = this.handleChangeSaida.bind(this);
+		this.handleChangeServico = this.handleChangeServico.bind(this);
+		this.handleChangeValor = this.handleChangeValor.bind(this);
+    this.handleChangeArr = this.handleChangeArr.bind(this);
+    this.handleChangeObs = this.handleChangeObs.bind(this);
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+
+
+	}
+
+  componentDidMount(){
+    let ret =  axios.get('/api/all').then(function (response) {
+      this.handleChangeArr(response);
+    }.bind(this))
+
+  }
+
+ 
+
+
+
+  handleChangeArr(event){
+    this.setState({arr: JSON.parse(event.data)});
+    console.log(this.state.arr);
+  }
+
+  handleChangeClient(event) {    
+  	this.setState({cliente: event.target.value});
+  }
+	
+  handleChangeContato(event) {    
+  	this.setState({contato: event.target.value});
+  }
+
+  handleChangeEntrada(event) {    
+  	this.setState({entrada: event.target.value});
+  }
+  handleChangePago(event) { 
+  	if (event.target.value == "pago")   
+  		this.setState({pago: true});
+  	else
+  		this.setState({pago: false});
+
+  }
+  handleChangeSaida(event) {    
+  	this.setState({saida: event.target.value});
+  }
+  handleChangeServico(event) {    
+  	this.setState({servico: event.target.value});
+  }
+
+  handleChangeValor(event) {    
+  	this.setState({valor: event.target.value});
+  }
+  handleChangeObs(event) {    
+    this.setState({obs: event.target.value});
+  }
+
+  handleDelete(id){
+
+    let r = '/api/delete/' + id;
+
+    axios.get(r).then(function (response) {
+      window.location.reload(false)
+    })
+
+
+  }
+
+  
+  
+
+  handleSubmit(event){
+
+		let ret = axios.post('/api/create', {
+	    cliente: this.state.cliente,
+	    contato: this.state.contato,
+	    entrada: this.state.entrada,
+	    saida: this.state.saida,
+	    pago: this.state.pago,
+	    servico: this.state.servico,
+	    valor: this.state.valor,
+      observacao: this.state.obs
+
+	  })
+	  .then(function (response) {
+	    window.location.reload(false) 	
+	  })  
+
+  
+  event.preventDefault();
+  }
+
+
+
+
+	render()	{
+
+
+
+		return(
+      <>
+
+		  <form onSubmit={this.handleSubmit}>
+        <label>
+          Cliente:
+
+          <input type="text" value={this.state.cliente} onChange={this.handleChangeClient} />
+        </label>
+        <label>
+          Contato:
+
+          <input type="text" value={this.state.contato} onChange={this.handleChangeContato} />
+        </label>
+        <label>
+          Entrada:
+          <input type="date" value={this.state.entrada} onChange={this.handleChangeEntrada} />
+         
+        </label>
+        <label>
+          Saida:
+          <input type="date" value={this.state.saida} onChange={this.handleChangeSaida} />
+         
+        </label>
+        <label>
+          Serviço:
+          <textarea value={this.state.servico} onChange={this.handleChangeServico} />        
+        </label>
+
+        <label>
+          Obs:
+          <textarea value={this.state.obs} onChange={this.handleChangeObs} />        
+        </label>
+
+        <label>
+          Status:
+         <select onChange={this.handleChangePago}>
+  				<option value="pago">Pendente</option>
+  				<option value="pendente">Pago</option>
+  			</select>	
+         
+        </label>
+        <label>
+          Valor:
+          <input type="number" value={this.state.valor} onChange={this.handleChangeValor} />
+         
+        </label>
+        
+        <input type="submit" value="Enviar" />
+      </form> 
+         <TableContainer component={Paper}  >
+        <Table size="small" aria-label="a dense table" >
+          <TableHead>
+            <TableRow>
+              <TableCell >Cliente</TableCell>
+              <TableCell >Contato</TableCell>
+              <TableCell >Serviço</TableCell>
+              <TableCell >Entrada</TableCell>
+              <TableCell >Saida</TableCell>
+              <TableCell >Valor</TableCell>
+              <TableCell >Pago</TableCell>
+              <TableCell >Obs</TableCell>
+              <TableCell >Ação</TableCell>
+
+            </TableRow>
+          </TableHead>
+        <TableBody>
+          {this.state.arr.map((row) => (
+            <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+                {row.cliente}
+              </TableCell>
+             
+              <TableCell >{row.contato}</TableCell>
+              <TableCell >{row.servico}</TableCell>
+              <TableCell >{row.entrada}</TableCell>
+              <TableCell >{row.saida}</TableCell>
+              <TableCell >{row.valor}</TableCell>
+              <TableCell >{row.pago? "Sim" : "Não"}</TableCell>
+              <TableCell >{row.observacao}</TableCell>
+              <TableCell ><a onClick={()=> this.handleDelete(row.id)}>Excluir</a> | <Dialogo/> </TableCell>
+
+            </TableRow>
+          ))}
+        </TableBody>
+        </Table> 
+      </TableContainer>   
+     
+
+
+  
+
+    </>
+  
+
+		);
+
+
+	}
+
+}
+
+export default Atendimento;
---
+++ lib/abel/atendimento.ex
@@ -0,0 +1,77 @@
+defmodule Abel.Atendimento do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Abel.Repo
+  alias Abel.Atendimento
+
+  schema "atendimentos" do
+    field :cliente, :string
+    field :contato, :string
+    field :entrada, :naive_datetime
+    field :pago, :boolean, default: false
+    field :saida, :naive_datetime
+    field :servico, :string
+    field :valor, :float
+    field :observacao, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(atendimento, attrs) do
+    atendimento
+    |> cast(attrs, [:cliente, :servico, :entrada, :saida, :contato, :valor, :pago, :observacao])
+   # |> validate_required([:cliente, :servico, :entrada, :saida, :contato, :valor, :pago])
+  end
+
+  def create(cliente) do
+    
+    changeset = cliente
+                |> Map.update!("entrada" , fn x -> 
+
+                  x
+                  |> Date.from_iso8601!
+                  |> NaiveDateTime.new(~T[00:00:00])
+                  |> elem(1) 
+
+                end)    
+                |> Map.update!("saida" , fn x -> 
+
+                  x
+                  |> Date.from_iso8601!
+                  |> NaiveDateTime.new(~T[00:00:00])
+                  |> elem(1) 
+
+                end) 
+
+    %Atendimento{}
+    |> Atendimento.changeset(changeset)          
+    |> Repo.insert
+                  
+
+
+  end
+
+
+  def edit(cliente) do
+    
+
+  end
+
+  def delete(id) do
+
+    int_id = id 
+              |> String.to_integer
+    
+    Atendimento
+    |> Repo.get(int_id)
+    |> Repo.delete
+
+  end
+
+  def all do
+    Atendimento
+    |> Repo.all    
+
+  end
+end
